1ea1175920b4eee5453b789fb281c49ef320c1ad
hannya <hannyaly2@gmail.com>
2022-07-13 18:35:18 -0400

    fix: change header for signin title

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,24 +1,40 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
 import {
   auth,
   signInWithGooglePopup,
+  // signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth);
+  //   if (response) {
+  //     const userDocRef = await createUserDocumentFromAuth(response.user);
+  //   }
+  // }, []);
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  //   console.log(user);
+  // };
   return (
-    <h1>
-      <div>Sign in </div>
+    <div>
+      <h1>Sign in </h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
-
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
       <SignUpForm />
-    </h1>
+    </div>
   );
 };
 
