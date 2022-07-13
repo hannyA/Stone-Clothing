ffd8583db1202a3aa50dcc1bee19e4cc9c8270c1
hannya <hannyaly2@gmail.com>
2022-07-13 18:17:04 -0400

    feat: Add Sign Up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,23 @@
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <h1>
       <div>Sign in </div>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+
+      <SignUpForm />
     </h1>
   );
 };
