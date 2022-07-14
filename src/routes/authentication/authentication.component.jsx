3670823dde4f3b8e0a8922f20e4ba51bfc8ed5f1
hannya <hannyaly2@gmail.com>
2022-07-14 09:50:20 -0400

    refactor: Rename Signin Component to Authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -10,7 +10,7 @@ import {
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
   // useEffect(async () => {
   //   const response = await getRedirectResult(auth);
   //   if (response) {
@@ -39,4 +39,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Authentication;
