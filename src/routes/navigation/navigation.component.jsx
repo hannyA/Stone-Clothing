6cebfa73f4738164bb59a4c19130a826b39cddaa
hannya <hannyaly2@gmail.com>
2022-07-14 22:10:17 -0400

    feat: Add context signout

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,10 +4,16 @@ import { Link } from "react-router-dom";
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
   // console.log(currentUser);
+
+  const signOutHandler = async () => {
+    const response = await signOutUser();
+    setCurrentUser(null);
+  };
 
   return (
     <Fragment>
@@ -20,7 +26,9 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className='nav-link'>SIGN OUT</span>
+            <span className='nav-link' onClick={signOutHandler}>
+              SIGN OUT
+            </span>
           ) : (
             <Link className='nav-link' to='/auth'>
               SIGN IN
