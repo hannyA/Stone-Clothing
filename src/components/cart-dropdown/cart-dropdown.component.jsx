625b820715049eb0fbe93e23289f8e0e913f519e
hannya <hannyaly2@gmail.com>
2022-07-16 10:37:15 -0400

    feat: Add checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,10 +5,14 @@ import "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import CartItem from "../cart-item/cart-item.component";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-
+  const navigate = useNavigate();
+  const gotoCheckoutHandler = () => {
+    navigate("/checkout");
+  };
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -16,7 +20,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={gotoCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
