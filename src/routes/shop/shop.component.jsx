e2c79f8d7e8c5af94b84e88319dc1f5d8bb6debc
hannya <hannyaly2@gmail.com>
2022-07-16 20:26:31 -0400

    refactor: Limit number of items to 4 under each category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,30 +1,24 @@
 import { useContext, Fragment } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+// import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
+    <div className='shop-container'>
       {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+
         return (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-
-            <div className='products-container'>
-              {categoriesMap[title].map((product) => {
-                const { id } = product;
-
-                return <ProductCard key={id} product={product} />;
-              })}
-            </div>
-          </Fragment>
+          <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </>
+    </div>
   );
 };
 
