ae559b68818820cc214f9bfda22e6cf36ee44be7
hannya <hannyaly2@gmail.com>
2022-07-17 23:25:03 -0400

    feat: Add route to categories page from main page links

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   DirecotryItemContainer,
   BackgroundImage,
@@ -5,9 +7,12 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { id, imageUrl, title } = category;
+  const { id, imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirecotryItemContainer key={id}>
+    <DirecotryItemContainer key={id} onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
