54658ac02213c4f0ae700bef66d9f3ac151283f6
hannya <hannyaly2@gmail.com>
2022-07-27 17:08:42 -0400

    refactor: Convert checkout scss file to use styled component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,6 +7,12 @@ import {
 } from "../../store/cart/cart.selector";
 
 import "./checkout.styles.scss";
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles";
 
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
@@ -14,30 +20,30 @@ const Checkout = () => {
 
   return (
     <>
-      <div className='checkout-container'>
-        <div className='checkout-header'>
-          <div className='header-block'>
+      <CheckoutContainer>
+        <CheckoutHeader>
+          <HeaderBlock>
             <span>Product</span>
-          </div>
-          <div className='header-block'>
+          </HeaderBlock>
+          <HeaderBlock>
             <span>Description</span>
-          </div>
-          <div className='header-block'>
+          </HeaderBlock>
+          <HeaderBlock>
             <span>Quantity</span>
-          </div>
-          <div className='header-block'>
+          </HeaderBlock>
+          <HeaderBlock>
             <span>Price</span>
-          </div>
-          <div className='header-block'>
+          </HeaderBlock>
+          <HeaderBlock>
             <span>Remove</span>
-          </div>
-        </div>
+          </HeaderBlock>
+        </CheckoutHeader>
         {cartItems.map((cartItem) => (
           <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
-        <span className='total'>Total: ${cartTotal}</span>
-      </div>
-      <PaymentForm />
+        <Total>Total: ${cartTotal}</Total>
+        <PaymentForm />
+      </CheckoutContainer>
     </>
   );
 };
